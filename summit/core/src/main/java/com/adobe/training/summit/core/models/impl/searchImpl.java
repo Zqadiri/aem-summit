72d51587aa0c62eadbe4fb5858e190e8a24e8ffe
zqadiri <zqadiri@student.1337.ma>
2023-05-30 01:12:21 +0100

    done with the sling, need to build the query to get the pages

--- summit/core/src/main/java/com/adobe/training/summit/core/models/impl/searchImpl.java
+++ summit/core/src/main/java/com/adobe/training/summit/core/models/impl/searchImpl.java
@@ -1,21 +1,59 @@
 package com.adobe.training.summit.core.models.impl;
 import java.util.List;
+
+import javax.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import com.adobe.training.summit.core.models.search;
+import com.adobe.training.summit.core.models.Search;
+
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(
-        adaptables = {SlingHttpServletRequest.class},
-        adapters = {search.class},
-        resourceType = {searchImpl.RESOURCE_TYPE},
-        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+    adaptables = { SlingHttpServletRequest.class }, 
+    adapters = { Search.class }, 
+    resourceType = {searchImpl.RESOURCE_TYPE }, 
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
-public class searchImpl implements search {
-    protected static final String RESOURCE_TYPE = "wknd/components/byline";
+public class searchImpl implements Search {
+    // private static final Logger LOG = LoggerFactory.getLogger(AuthorImpl.class);
+    protected static final String RESOURCE_TYPE = "summit/components/searchComponent";
+
+    @Self
+    private SlingHttpServletRequest req;
+
+    @Self
+    private SlingHttpServletResponse res;
+
+    @Inject
+    @Default(values = "AEM")
+    private String pname;
+
+    @ValueMapValue
+    private List<String> pages;
+
+    @Override
+    public String getPname(){
+        pname = req.getParameter("pname");
+        return pname;
+    }
 
     @Override
     public List<String> getPages() {
-        return null;
+        // if (pages != null) {
+            pages = new ArrayList<>();
+            pages.add("A");
+            pages.add("E");
+            pages.add("U");
+            return pages;
+        // } else {
+        //     return Collections.emptyList();
+        // }
     }
 }
--- summit/core/src/main/java/com/adobe/training/summit/core/models/search.java
+++ summit/core/src/main/java/com/adobe/training/summit/core/models/search.java
@@ -1,8 +1,9 @@
 package com.adobe.training.summit.core.models;
 import java.util.List;
 
-public interface search {
-    /***
+public interface Search {
+    String getPname();
+    /**
     * @return a list of Pages.
     */
     List<String> getPages();
